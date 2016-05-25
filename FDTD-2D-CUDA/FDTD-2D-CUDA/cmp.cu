c8f77216b721dee960df475ae68b80471b2b1e75
Xuanyu Wang <xuanyu.wang@outlook.com>
2016-05-25 10:35:14 +0800

    compute any size FDTD

--- FDTD-2D-CUDA/FDTD-2D-CUDA/cmp.cu
+++ FDTD-2D-CUDA/FDTD-2D-CUDA/cmp.cu
@@ -1,13 +1,13 @@
 #include "cmp.cuh"
 
-__global__ 
+__global__
 void src_cmp_kernel(
-int current_timestep, 
-float* dev_Ez, 
+int current_timestep,
+float* dev_Ez,
 int size_Ez_x, int size_Ez_y, float dt, int ele_ex
 )
 {
-	float T,T0;
+	float T, T0;
 	float vt, val_src, time;
 	int src_pos_x, src_pos_y;
 
@@ -25,39 +25,47 @@ int size_Ez_x, int size_Ez_y, float dt, int ele_ex
 	dev_Ez[src_pos_y * ele_ex + src_pos_x] = val_src;
 }
 
-__global__ 
+__global__
 void Ez_cmp_kernel(
-					float* Ez, float* Hx, float* Hy, 
-					float coe_Ez, int size_Ez_x, int size_Ez_y, 
-					int ele_ex, int ele_hx, int ele_hy
+float* Ez, float* Hx, float* Hy,
+float coe_Ez, int size_Ez_x, int size_Ez_y,
+int ele_ex, int ele_hx, int ele_hy
 )
 {
-	int i, j;
+	int x, y;
+	int tid, number;
+	tid = threadIdx.x + blockIdx.x*blockDim.x;
 	float dif_Hy, dif_Hx;
-	for (i = 1; i < size_Ez_y - 1; i++){
-		for (j = 1; j < size_Ez_x - 1; j++){
-			//Hy(i,j)	-	Hy(i-1,j)
-			dif_Hy = Hy[i*ele_hy + j] - Hy[(i - 1)* ele_hy + j];
-			//Hx(i,j-1)	-	Hx(i,j)
-			dif_Hx = Hx[i*ele_hx + (j - 1)] - Hx[i*ele_hx + j];
-			Ez[i*ele_hx + j] += coe_Ez * (dif_Hx + dif_Hy);
-		}
+	while (tid < ele_ex*size_Ez_y)
+	{
+		number = tid + 1;
+		y = number % ele_ex;//row
+		x = number - (y*ele_ex);//column
+		//Hy(i,j)	-	Hy(i-1,j)
+		dif_Hy = Hy[y*ele_hy + x] - Hy[(y - 1)* ele_hy + x];
+		//Hx(i,j-1)	-	Hx(i,j)
+		dif_Hx = Hx[y*ele_hx + (x - 1)] - Hx[y*ele_hx + x];
+		Ez[y*ele_ex + x] += coe_Ez * (dif_Hx + dif_Hy);
+		tid += blockDim.x*gridDim.x;
 	}
 }
 
 __global__ void Hx_cmp_kernel(
-	float* Ez, float* Hx, 
+	float* Ez, float* Hx,
 	int size_Hx_x, int size_Hx_y,
 	float coe_H, int ele_ex, int ele_hx
-)
+	)
 {
-	int i, j;
-
-	for ( i = 0; i < size_Hx_y; i++){
-		for ( j = 0; j < size_Hx_x; j++){
-			Hx[i*ele_hx + j] += coe_H *
-				(Ez[i*ele_ex + j] - Ez[i*ele_ex + (j + 1)]);
-		}
+	int x, y;
+	int tid, number;
+	tid = threadIdx.x + blockIdx.x*blockDim.x;
+	while (tid < ele_hx*size_Hx_y)
+	{
+		number = tid + 1;
+		y = number % ele_hx;
+		x = number - (y*ele_hx);
+		Hx[y*ele_hx + x] += coe_H *(Ez[y*ele_ex + x] - Ez[y*ele_ex + (x + 1)]);
+		tid += blockDim.x * gridDim.x;
 	}
 }
 
@@ -65,20 +73,23 @@ __global__ void Hy_cmp_kernel(
 	float* Ez, float* Hy,
 	int size_Hy_x, int size_Hy_y,
 	float coe_H, int ele_ex, int ele_hy
-)
+	)
 {
-	int i, j;
-
-	for ( i = 0; i < size_Hy_y; i++){
-		for ( j = 0; j < size_Hy_x; j++){
-			Hy[i*ele_hy + j] += 
-				coe_H * (Ez[(i + 1)*ele_ex + j] - Ez[i*ele_ex + j]);
-		}
+	int x, y;
+	int tid, number;
+	tid = threadIdx.x + blockIdx.x*blockDim.x;
+	while (tid < ele_hy * size_Hy_y)
+	{
+		number = tid + 1;
+		y = number % ele_hy;
+		x = number - (y*ele_hy);
+		Hy[y*ele_hy + x] += coe_H * (Ez[(y + 1)*ele_ex + x] - Ez[y*ele_ex + x]);
+		tid += blockDim.x * gridDim.x;
 	}
 }
 
 __global__ void Ez_boundary_PEC(
-	float* Ez, 
+	float* Ez,
 	int size_Ez_x, int size_Ez_y, int ele_ex
 	)
 {
@@ -99,52 +110,62 @@ __global__ void Ez_boundary_PEC(
 
 __global__
 void Ez_MUR_u(
-float* Ez, float* E_bd_u, float* E_nbd_u, 
+float* Ez, float* E_bd_u, float* E_nbd_u,
 int size_Ez_x, int size_Ez_y, float coe_MUR, int ele_ex
 )
 {
-	int i;
-	for ( i = 0; i < size_Ez_x; i++){
-		Ez[(size_Ez_y - 1) * ele_ex + i] = E_nbd_u[i]
-			+ coe_MUR * (Ez[(size_Ez_y - 2) * ele_ex + i]
-			- E_bd_u[i]);
-		E_nbd_u[i] = Ez[(size_Ez_y - 2) * ele_ex + i];
-		E_bd_u[i] = Ez[(size_Ez_y - 1) * ele_ex + i];
+	int tid;
+	tid = threadIdx.x + blockIdx.x*blockDim.x;
+	while (tid < size_Ez_x){
+		Ez[(size_Ez_y - 1) * ele_ex + tid] = E_nbd_u[tid]
+			+ coe_MUR * (Ez[(size_Ez_y - 2) * ele_ex + tid]
+			- E_bd_u[tid]);
+		E_nbd_u[tid] = Ez[(size_Ez_y - 2) * ele_ex + tid];
+		E_bd_u[tid] = Ez[(size_Ez_y - 1) * ele_ex + tid];
+		tid += blockDim.x * gridDim.x;
 	}
 }
 
 __global__
 void Ez_MUR_d(
-float* Ez, float* E_bd_d, float* E_nbd_d, 
+float* Ez, float* E_bd_d, float* E_nbd_d,
 int size_Ez_x, int size_Ez_y, float coe_MUR, int ele_ex
 )
 {
-	int i;
-	for ( i = 0; i < size_Ez_x; i++){
-		Ez[i] = E_nbd_d[i] + coe_MUR * (Ez[1*ele_ex + i]
-			- E_bd_d[i]);
-		E_nbd_d[i] = Ez[1*ele_ex + i];
-		E_bd_d[i] = Ez[i];
+	int tid;
+	tid = threadIdx.x + blockIdx.x*blockDim.x;
+	while (tid < size_Ez_x)
+	{
+		Ez[tid] = E_nbd_d[tid] + coe_MUR * (Ez[1 * ele_ex + tid]
+			- E_bd_d[tid]);
+		E_nbd_d[tid] = Ez[1 * ele_ex + tid];
+		E_bd_d[tid] = Ez[tid];
+		tid += blockDim.x * gridDim.x;
 	}
 }
 
 __global__
 void Ez_MUR_lr(
-float* Ez, float* E_bd_l, float* E_nbd_l, 
+float* Ez, float* E_bd_l, float* E_nbd_l,
 float* E_bd_r, float* E_nbd_r,
 int size_Ez_x, int size_Ez_y, float coe_MUR, int ele_ex
 )
 {
-	for (int i = 0; i < size_Ez_y; i++){
+	int tid;
+	tid = threadIdx.x + blockIdx.x * blockDim.x;
+	while (tid < size_Ez_y)
+	{
 		//left
-		Ez[i*ele_ex + 0] = E_nbd_l[i] + coe_MUR *
-			(Ez[i * ele_ex + 1] - E_bd_l[i]);
-		E_nbd_l[i] = Ez[i * ele_ex + 1];
-		E_bd_l[i] = Ez[i * ele_ex + 0];
+		Ez[tid*ele_ex + 0] = E_nbd_l[tid] + coe_MUR *
+			(Ez[tid * ele_ex + 1] - E_bd_l[tid]);
+		E_nbd_l[tid] = Ez[tid * ele_ex + 1];
+		E_bd_l[tid] = Ez[tid * ele_ex + 0];
 		//right
-		Ez[i* ele_ex + (size_Ez_x - 1)] = E_nbd_r[i] + coe_MUR *
-			(Ez[i * ele_ex + (size_Ez_x - 2)] - E_bd_r[i]);
-		E_nbd_r[i] = Ez[i * ele_ex + (size_Ez_x - 2)];
-		E_bd_r[i] = Ez[i* ele_ex + (size_Ez_x - 1)];
+		Ez[tid* ele_ex + (size_Ez_x - 1)] = E_nbd_r[tid] + coe_MUR *
+			(Ez[tid * ele_ex + (size_Ez_x - 2)] - E_bd_r[tid]);
+		E_nbd_r[tid] = Ez[tid * ele_ex + (size_Ez_x - 2)];
+		E_bd_r[tid] = Ez[tid* ele_ex + (size_Ez_x - 1)];
+
+		tid += blockDim.x * gridDim.x;
 	}
 }
--- FDTD-2D-CUDA/FDTD-2D-CUDA/main.cu
+++ FDTD-2D-CUDA/FDTD-2D-CUDA/main.cu
@@ -9,26 +9,26 @@ using namespace std;
 
 int main()
 {
-	src source(30,30,400);
+	src source(100, 100, 300);
 	H Hxy(source);
 	E Ez(source);
 	int i;
 
 	source.src_checkout();
 
-
+	cout << Ez.ele_Ez << "\t" << Ez.size_Ez_x << endl;
 	for (i = 0; i < source.size_time; i++){
-		cudaMemcpy2D(Ez.dev_Ez, Ez.pitch_Ez, Ez.Ez, Ez.size_Ez_x*sizeof(float), Ez.size_Ez_x*sizeof(float), Ez.size_Ez_y, cudaMemcpyHostToDevice);
+		//cudaMemcpy2D(Ez.dev_Ez, Ez.pitch_Ez, Ez.Ez, Ez.size_Ez_x*sizeof(float), Ez.size_Ez_x*sizeof(float), Ez.size_Ez_y, cudaMemcpyHostToDevice);
 
-		Hy_cmp_kernel<<<1,1>>>(Ez.dev_Ez, Hxy.dev_Hy, Hxy.size_Hy_x, Hxy.size_Hy_y, Hxy.coe_H, Ez.ele_Ez, Hxy.ele_Hy);
-		Hx_cmp_kernel<<<1,1>>>(Ez.dev_Ez, Hxy.dev_Hx, Hxy.size_Hx_x, Hxy.size_Hx_y, Hxy.coe_H, Ez.ele_Ez, Hxy.ele_Hy);
-		Ez_cmp_kernel<<<1,1>>>(Ez.dev_Ez, Hxy.dev_Hx, Hxy.dev_Hy, Ez.coe_Ez, Ez.size_Ez_x, Ez.size_Ez_y, Ez.ele_Ez, Hxy.ele_Hx, Hxy.ele_Hy);
+		Hy_cmp_kernel << <50, 50 >> >(Ez.dev_Ez, Hxy.dev_Hy, Hxy.size_Hy_x, Hxy.size_Hy_y, Hxy.coe_H, Ez.ele_Ez, Hxy.ele_Hy);
+		Hx_cmp_kernel << <50, 50 >> >(Ez.dev_Ez, Hxy.dev_Hx, Hxy.size_Hx_x, Hxy.size_Hx_y, Hxy.coe_H, Ez.ele_Ez, Hxy.ele_Hy);
+		Ez_cmp_kernel << <50, 50 >> >(Ez.dev_Ez, Hxy.dev_Hx, Hxy.dev_Hy, Ez.coe_Ez, Ez.size_Ez_x, Ez.size_Ez_y, Ez.ele_Ez, Hxy.ele_Hx, Hxy.ele_Hy);
 		//Ez_boundary_PEC<<<1,1>>>(Ez.dev_Ez, Ez.size_Ez_x, Ez.size_Ez_y, Ez.ele_Ez);
-		Ez_MUR_u<<<1,1>>>(Ez.dev_Ez, Ez.E_bd_u, Ez.E_nbd_u, Ez.size_Ez_x, Ez.size_Ez_y, Ez.coe_MUR, Ez.ele_Ez);
-		Ez_MUR_d<<<1,1>>>(Ez.dev_Ez, Ez.E_bd_d, Ez.E_nbd_d, Ez.size_Ez_x, Ez.size_Ez_y, Ez.coe_MUR, Ez.ele_Ez);
-		Ez_MUR_lr<<<1,1>>>(Ez.dev_Ez, Ez.E_bd_l, Ez.E_nbd_l, Ez.E_bd_r, Ez.E_nbd_r, Ez.size_Ez_x, Ez.size_Ez_y, Ez.coe_MUR, Ez.ele_Ez);
-		src_cmp_kernel<<<1,1>>>(i, Ez.dev_Ez, Ez.size_Ez_x, Ez.size_Ez_y, source.dt, Ez.ele_Ez);
-		
+		Ez_MUR_u << <50, 50 >> >(Ez.dev_Ez, Ez.E_bd_u, Ez.E_nbd_u, Ez.size_Ez_x, Ez.size_Ez_y, Ez.coe_MUR, Ez.ele_Ez);
+		Ez_MUR_d << <50, 50 >> >(Ez.dev_Ez, Ez.E_bd_d, Ez.E_nbd_d, Ez.size_Ez_x, Ez.size_Ez_y, Ez.coe_MUR, Ez.ele_Ez);
+		Ez_MUR_lr << <1, Ez.size_Ez_y >> >(Ez.dev_Ez, Ez.E_bd_l, Ez.E_nbd_l, Ez.E_bd_r, Ez.E_nbd_r, Ez.size_Ez_x, Ez.size_Ez_y, Ez.coe_MUR, Ez.ele_Ez);
+		src_cmp_kernel << <1, 1 >> >(i, Ez.dev_Ez, Ez.size_Ez_x, Ez.size_Ez_y, source.dt, Ez.ele_Ez);
+
 		cudaMemcpy2D(Ez.Ez, Ez.size_Ez_x*sizeof(float), Ez.dev_Ez, Ez.pitch_Ez, Ez.size_Ez_x*sizeof(float), Ez.size_Ez_y, cudaMemcpyDeviceToHost);
 		Ez.Ez_save2file();
 	}
